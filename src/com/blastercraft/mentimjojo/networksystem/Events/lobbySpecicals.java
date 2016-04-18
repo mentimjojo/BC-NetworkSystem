dfb41a24abc4bab3b6b23d0922ca82d93e628e30
mentimjojo <timnijborg@hotmail.nl>
2016-04-18 20:27:13 +0200

    Added events

---
+++ src/com/blastercraft/mentimjojo/networksystem/Events/lobbySpecicals.java
@@ -0,0 +1,52 @@
+package com.blastercraft.mentimjojo.networksystem.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class lobbySpecicals implements Listener{
+
+    // Hub location
+    Location location = new Location(Bukkit.getWorld("lobby2"), -252.500, 42, -979.300);
+
+    /*
+    Teleport on join
+    */
+    @EventHandler
+    public void DoStuffOnJoin(PlayerJoinEvent event){
+        // Teleport right location
+        event.getPlayer().teleport(location);
+    }
+
+    /*
+    Teleport on void
+    */
+    @EventHandler
+    public void teleportOnVoid(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            if (event.getCause().equals(EntityDamageEvent.DamageCause.VOID)) {
+                event.getEntity().teleport(location);
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    /*
+    Give jump/Run boost
+     */
+    @EventHandler
+    public void giveBoost(PlayerMoveEvent event) {
+        // Get player
+        Player player = event.getPlayer();
+        // Give Boost
+        player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 999999999, 3));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999999, 3));
+    }
+}
---
+++ src/com/blastercraft/mentimjojo/networksystem/Events/playerJoinQuit.java
@@ -0,0 +1,43 @@
+package com.blastercraft.mentimjojo.networksystem.events;
+
+import com.blastercraft.mentimjojo.networksystem.core.Settings;
+import com.blastercraft.mentimjojo.networksystem.core.Title;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class playerJoinQuit implements Listener {
+
+    /*
+    Teleport on join
+    */
+    @EventHandler
+    public void DoStuffOnJoin(PlayerJoinEvent event) {
+        // Set message null
+        event.setJoinMessage(null);
+        // Setup title
+        Title title = new Title("&B" + Settings.pluginServerName.toUpperCase(), "&AYou are on " + Settings.pluginServerName, 1, 3, 2);
+        title.setTitleColor(ChatColor.AQUA);
+        title.setSubtitleColor(ChatColor.GREEN);
+        // Send title
+        title.send(event.getPlayer());
+        // Check if can run
+        if (!Settings.exServers.contains(Settings.pluginServerName)) {
+            // Set right slot
+            event.getPlayer().getInventory().setHeldItemSlot(0);
+            // Clear inventory
+            event.getPlayer().getInventory().clear();
+        }
+    }
+
+    /*
+    On quit event
+    */
+    @EventHandler
+    public void DoStuffOnQuit(PlayerQuitEvent event) {
+        // Remove quit message
+        event.setQuitMessage(null);
+    }
+}
